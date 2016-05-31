87346f538f0b21fe8e175bbf300578f18c6eb63f
llcc402 <llcc402@163.com>
2016-06-01 09:13:31 +1000

    finish data_generate.m

--- data_generate.m
+++ data_generate.m
@@ -9,5 +9,21 @@
 
 function data = data_generate()
 
-x1 = randn(1, 200);
-x2 = randn(1, 200) + 5;
+x1 = randn(1, 100);
+x2 = randn(1, 100) + 5;
+x3 = randn(1, 100) - 5;
+x4 = randn(1, 100) - 10;
+x5 = randn(1, 100) + 10;
+x6 = randn(1, 100) + 15;
+
+data = zeros(2, 400);
+data(1,:) = [x1, x2, x3, x4];
+data(2,:) = [x1, x2, x5, x6];
+
+figure(1)
+hist(data(1,:), 50)
+title('The first group')
+
+figure(2)
+hist(data(2,:), 50)
+title('The second group')
